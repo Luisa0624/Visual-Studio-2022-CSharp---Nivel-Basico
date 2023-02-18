ee6fa0fb0e1203247fc65f55a5eb4127f67f0e09
Luisa Maria Gil Garcia <luisamariagilgarcia24@gmail.com>
2023-02-18 18:03:50 -0500

    first commit

---
+++ Solucion_CSharp_NivelBásico/Pj_HolaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pj_HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Bienvenidos al curso de C# - Nivel Basico");
+            Console.Write("Suma -> 5 + 6 ="); Console.WriteLine(5+6);
+            Console.Write("Resta -> 10 - 2 ="); Console.WriteLine(10-2);
+            Console.Write("Multiplicacion -> 8 x 3 ="); Console.WriteLine(8*3);
+            Console.Write("Division -> 10 / 3 ="); Console.WriteLine(10/3);
+            Console.Write("Residuo de division -> 10 / 3 ="); Console.WriteLine(10%3);
+            Console.ReadKey();
+
+        }
+    }
+}
